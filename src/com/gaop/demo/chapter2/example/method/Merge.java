7d97c16dc46086e211e160775debb681b45ffb35
gaopeng <gaopeng@danlu.com>
2018-02-08 09:33:45 +0800

    [添加归并排序的注释信息]

--- src/com/gaop/demo/chapter2/example/method/Merge.java
+++ src/com/gaop/demo/chapter2/example/method/Merge.java
@@ -2,6 +2,13 @@ package com.gaop.demo.chapter2.example.method;
 
 import com.gaop.demo.chapter2.example.BaseModel;
 
+/**
+ * @description
+ * 	归并排序,即将两个有序的数组归并称为一个更大的有序数组。算法最明显的特质是: 可以保证任意长度为N的数组排序所需时间和
+ * 	<b>NlogN</b>成正比,主要是缺点是所需的额外空间和N成正比.
+ * @author gao
+ *
+ */
 public class Merge extends BaseModel<Integer>{
 
 	@Override
