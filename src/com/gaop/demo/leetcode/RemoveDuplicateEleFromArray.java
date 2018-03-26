d42c0d37ebec139c9ad6800b19bb2b4cf3be709c
gaopeng <gp626676634@sina.com>
2018-03-27 00:04:48 +0800

    [leetcode-替换0元素]

--- src/com/gaop/demo/leetcode/RemoveDuplicateEleFromArray.java
+++ src/com/gaop/demo/leetcode/RemoveDuplicateEleFromArray.java
@@ -2,7 +2,9 @@ package com.gaop.demo.leetcode;
 
 /**
  * @description
- * 	删除数组中的重复元素,要求是原地删除并返回新的数组长度
+ * 	删除有序数组中的重复元素,要求是原地删除并返回新的数组长度。 <br>
+ * 	思路：数组本身已经是有序的了，因此我们可以在一趟遍历中就检测到所有的重复元素。使用一个虚拟指针来将这个数组中的所有元素都整理一遍，越过那些已经重复的元素。比较中出现相同的元素则不移动指针位置。比较出现前后两个位置不同的元素就将
+ *  这个位置插入不同的新元素并移动一位指针。
  * @author gao
  *
  */
@@ -20,24 +22,14 @@ public class RemoveDuplicateEleFromArray {
     		return 0;
     	if(nums.length == 1)
     		return 1;
-    	int repeatCount = 0; // 元素重复次数统计
-    	for (int k = 0; k < nums.length; k++) {
-    		int index = 0; // 用于重复检查的索引
-        	for (int i = k; i < nums.length; i++) {
-        		// 遍历数组，确认元素的唯一性
-    			if(nums[index] == nums[i])
-    			{
-    				// 检查到相等的元素,repeat元素后面的元素统一前移一位,统计字段+1
-    		        for(int j = i; j < nums.length - 1; j++)
-    		        {
-    		            nums[j] = nums[j+1];
-    		        }
-    				repeatCount ++;
-    			}
-    		}
-        	index++;
+		int index = 0; // 用于重复检查的索引
+    	for (int i = 1; i < nums.length; i++) {
+			if(nums[i] != nums[index])
+			{
+				nums[++index] = nums[i]; // 移动指针并将不同的元素插入
+			}
 		}
-    	return nums.length - repeatCount;
+    	return index + 1;
     }
     
     public static void main(String[] args) {
@@ -55,5 +47,6 @@ public class RemoveDuplicateEleFromArray {
     	show(arg2);
     	System.out.println(removeDuplicates(arg2));
     	show(arg2);
+    	
 	}
 }
